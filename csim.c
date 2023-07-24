5e98fc1b156edbb60a6881da6a38f4d9e5f490f1
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-07-25 00:02:12 +0300

    Initial commit

---
+++ csim.c
@@ -0,0 +1,13 @@
+#include "cachelab.h"
+#include <getopt.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+    printSummary(0, 0, 0);
+    return 0;
+}
