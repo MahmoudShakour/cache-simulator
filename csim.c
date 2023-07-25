538efb654e682ae2f85966a59245db4735920ecf
Mahmoud Abdulshakour <mahmoud.ali1842001@gmail.com>
2023-07-25 21:13:22 +0300

    Decode address to corresponding bits

--- csim.c
+++ csim.c
@@ -5,16 +5,25 @@
 #include <unistd.h>
 #include <stdio.h>
 
+const int numberOfAddressBits = 64;
+
 typedef struct
 {
     char type[5];
-    unsigned int address;
+    unsigned long address;
     int size;
 } Access;
 
+typedef struct
+{
 
+    int valid;
+    int tag;
+    int time_stamp;
 
-void parseArguments(int argc, char *argv[], int *numberOfSetBits, int *numberOfLines, int *numberOfBytes, char *fileName)
+} Line;
+
+void parseArguments(int argc, char *argv[], int *numberOfSetBits, int *numberOfLines, int *numberOfBlockBits, char *fileName)
 {
     int opt;
     while ((opt = getopt(argc, argv, "E:b:t:s:")) != -1)
@@ -27,7 +36,7 @@ void parseArguments(int argc, char *argv[], int *numberOfSetBits, int *numberOfL
         case 'E':
             *numberOfLines = atoi(optarg);
         case 'b':
-            *numberOfBytes = atoi(optarg);
+            *numberOfBlockBits = atoi(optarg);
         case 't':
             strcpy(fileName, optarg);
         }
@@ -51,22 +60,58 @@ void parseOperations(char *fileName, Access *operations, int *numberOfOperations
     *numberOfOperations = i;
 }
 
+void getAddressInfo(unsigned long address, int numberOfSetBits, int numberOfBlockBits, unsigned long *tag, unsigned long *set, unsigned long *blockOffset)
+{
+    for(int i=0;i<64;i++){
+        if(i<numberOfBlockBits){
+            (*blockOffset)|=((1LL<<i)&address);
+        }
+        else if((i>=numberOfBlockBits)&&(i<numberOfBlockBits+numberOfSetBits)){
+            (*set)|=((1LL<<i)&address);
+        }
+        else{
+            (*tag)|=((1LL<<i)&address);
+        }
+    }
+    (*set)>>=numberOfBlockBits;
+    (*tag)>>=(numberOfBlockBits+numberOfSetBits);
+}
+
 int main(int argc, char *argv[])
 {
 
-    int numberOfSetBits, numberOfLines, numberOfBytes, numberOfOperations;
+    int numberOfSetBits, numberOfLines, numberOfBlockBits, numberOfOperations;
     char fileName[30];
     Access operations[20];
 
-    parseArguments(argc, argv, &numberOfSetBits, &numberOfLines, &numberOfBytes, fileName);
+    parseArguments(argc, argv, &numberOfSetBits, &numberOfLines, &numberOfBlockBits, fileName);
 
-    printf("%d %d %d %s\n", numberOfSetBits, numberOfLines, numberOfBytes, fileName);
+    printf("%d %d %d %s\n", numberOfSetBits, numberOfLines, numberOfBlockBits, fileName);
 
     parseOperations(fileName, operations, &numberOfOperations);
 
     for (int i = 0; i < numberOfOperations; i++)
     {
-        printf(" %s %x,%d\n", operations[i].type, operations[i].address, operations[i].size);
+        printf(" %s %lu,%d\n", operations[i].type, operations[i].address, operations[i].size);
+        
+    }
+
+    Line cache[1 << numberOfSetBits][numberOfLines];
+
+    for(int i=0;i<1<<numberOfSetBits;i++){
+        for(int j=0;j<numberOfLines;j++){
+            cache[i][j].valid=0;
+            printf("%d\n",cache[i][j].valid);
+        }
+    }
+
+    for (int i = 0; i < numberOfOperations; i++)
+    {
+        unsigned long tag=0,set=0,blockOffset=0;
+        printf("%lu\n",operations[i].address);
+        getAddressInfo(operations[i].address,numberOfSetBits,numberOfBlockBits,&tag,&set,&blockOffset);
+        printf("%lu %lu %lu\n",tag,set,blockOffset);
+        
     }
 
     // printSummary(0, 0, 0);
